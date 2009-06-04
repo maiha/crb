6fb97e3588b031612274e5ab83dad96bb4f9ab00
maiha <maiha@wota.jp>
2009-06-05 06:46:33 +0900

    Support hooks

--- lib/crb.rb
+++ lib/crb.rb
@@ -5,19 +5,34 @@ require 'irb'
 
 class CRB < Cucumber::Cli::Main
   extend IRB
-  extend Cucumber::World
+end
+CRB.step_mother = self
 
+class CRB
   class << self
+    attr_accessor :irb
+
     def world
-      step_mother.current_world
+      unless @world
+        @world = CRB.step_mother
+        def @world.to_s
+          "World"
+        end
+      end
+      @world
     end
 
     def start(args)
       IRB.setup(__FILE__)
-      ws  = WorkSpace.new(binding)
-      irb = Irb.new(ws)
+      IRB.conf[:CONTEXT_MODE] = 0
+      ws  = WorkSpace.new(world)
+      CRB.irb = Irb.new(ws)
       IRB.module_eval do
-        @CONF[:MAIN_CONTEXT] = irb.context
+        @CONF[:MAIN_CONTEXT] = CRB.irb.context
+      end
+
+      step_mother.World do
+        world
       end
 
       execute(args)
@@ -29,9 +44,33 @@ class CRB < Cucumber::Cli::Main
       trap("SIGINT") do
         irb.signal_handle
       end
-    
-      catch(:IRB_EXIT) do
-        irb.eval_input
+
+      around_hooks do
+        catch(:IRB_EXIT) do
+          irb.eval_input
+        end
+      end
+    end
+
+    def around_hooks(&block)
+      step_mother.hooks[:before].each do |hook|
+        hook.execute(world)
+      end
+
+      block.call
+
+      step_mother.hooks[:after].each do |hook|
+        hook.execute(world)
+      end
+    end
+
+    def alias_adverbs
+      world.instance_eval do
+        hash = Cucumber.keyword_hash
+        keywords = %w{given when then and but}.map{|keyword| hash[keyword].split('|')}.flatten
+        keywords.each do |name|
+          instance_eval "alias :'#{name}' :__cucumber_invoke"
+        end
       end
     end
 
@@ -39,36 +78,27 @@ class CRB < Cucumber::Cli::Main
       require 'webrat'
       require 'webrat/core/matchers'
 
-      extend ::Webrat::Methods 
-      extend ::Webrat::Matchers
+      world.instance_eval do
+        extend ::Webrat::Methods 
+        extend ::Webrat::Matchers
 
-      if mode || !Webrat.configure.mode
-        Webrat.configure.mode = (mode || :mechanize)
-        if Webrat.configure.mode == :mechanize
-          webrat_session.mechanize.user_agent_alias = 'Windows Mozilla'
-        end
-      end
-
-      %w{response post}.each do |m|
-        unless respond_to?(m, true)
-          instance_eval <<-EOF
-            def self.#{m}(*args, &blk)
-              webrat.#{m}(*args, &blk)
-            end
-          EOF
-        end
-      end
-    end
-
-    def alias_adverbs
-      hash = Cucumber.keyword_hash
-      keywords = %w{given when then and but}.map{|keyword| hash[keyword].split('|')}.flatten
-      keywords.each do |name|
-        instance_eval <<-EOF
-          def CRB.#{name}(*args, &blk)
-            world.#{name}(*args, &blk)
+        if mode || !Webrat.configure.mode
+          Webrat.configure.mode = (mode || :mechanize)
+          if Webrat.configure.mode == :mechanize
+            webrat_session.mechanize.user_agent_alias = 'Windows Mozilla'
           end
-        EOF
+        end
+
+        %w{response post}.each do |m|
+          unless respond_to?(m, true)
+            instance_eval <<-EOF
+              def #{m}(*args, &blk)
+                webrat.#{m}(*args, &blk)
+              end
+            EOF
+          end
+        end
+
       end
     end
   end
@@ -85,6 +115,18 @@ class CRB < Cucumber::Cli::Main
     visitor = configuration.build_formatter_broadcaster(step_mother)
     step_mother.visitor = visitor # Needed to support World#announce
   end
+
+  module ExecuteProc
+    def execute(world)
+      world.instance_eval(&@proc)
+    end
+  end
+
 end
 
 CRB.step_mother = self
+
+Cucumber::StepMother::Hook.class_eval do
+  include CRB::ExecuteProc
+end
+
